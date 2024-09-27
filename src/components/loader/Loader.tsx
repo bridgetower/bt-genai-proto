0a18dcd53ec97d0980f6eb593ef6ebf892569c72
rupeshintervision <rupesh.verma@intervision.com>
2024-09-27 18:48:03 +0530

    logo image update

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -10,7 +10,7 @@ export const Loader: React.FC<LoaderWrapperProps> = ({ show }) => {
       {show ? (
         <div className="fixed inset-0 flex items-center justify-center bg-white/30 z-50">
           <div className="relative flex items-center justify-center w-64 h-64">
-            <img src="/images/logo.png" alt="Loading" className="w-16 h-w-16 rounded-full animate-zoom" />
+            <img src="/logo512.png" alt="Loading" className="w-16 h-w-16 rounded-full animate-zoom" />
             {/* Rotating Circle */}
             <div className="absolute border-t-4 border-amber-500 border-solid rounded-full w-20 h-20 animate-spin"></div>
           </div>
