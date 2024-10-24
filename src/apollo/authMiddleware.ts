81377be92e06a5b796814fcf9fa2c8eb84c53def
rupeshintervision <rupesh.verma@intervision.com>
2024-10-24 20:51:06 +0530

    graphql url updated

--- src/apollo/authMiddleware.ts
+++ src/apollo/authMiddleware.ts
@@ -1,7 +1,7 @@
 import { HttpLink } from "@apollo/client";
 
 export const AuthMiddleware = new HttpLink({
-  uri: "https://s2nmqqmjdzetpk53p5wsutbcqy.appsync-api.us-east-1.amazonaws.com/graphql",
+  uri: "https://dtnrvkna4reublxln46zchrfii.appsync-api.us-east-1.amazonaws.com/graphql",
   headers: {
     Authorization: process.env.REACT_APP_TANENT_API_KEY || ""
   }
